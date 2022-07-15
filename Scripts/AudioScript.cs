4bb3aa857256a30d52963d9f45414957d1200f40
KluivertOk <93863160+KluivertOk@users.noreply.github.com>
2022-07-15 18:39:49 +0100

    Visuals

---
+++ Scripts/AudioScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+    public AudioSource backgroundAudio;
+
+    void Start() {
+        backgroundAudio.Play();
+     
+    }
+
+}
